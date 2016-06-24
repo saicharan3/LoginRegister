41c1dc581cecfe4f4df6820f0175896d94f87c4f
saicharan3 <charankatikala369@gmail.com>
2016-06-24 14:14:36 +0530

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,15 +1,18 @@
 <?php
-    $con=mysqli_connect("localhost","my_user","my_password","my_db");
+    $con=mysqli_connect("http://backloggers3.net23.in","a4794786_BUNNY","Bhargavi987","a4794786_OUFEED");
     
-    $name = $_POST["name"];
-    $age = $_POST["age"];
+    $email = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
+    $rpassword = $_POST["rpassword"];
     
-    $statement = mysqli_prepare($con, "INSERT INTO User (name, age, username, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    $statement = mysqli_prepare($con, "INSERT INTO a4794786_BUNNY (email,username,password, rpassword) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $email, $username, $password, $rpassword);
     mysqli_stmt_execute($statement);
-    
+    $response = array();
+    $response["success"] = true;  
+     
+    echo json_encode($response);
     mysqli_stmt_close($statement);
     
     mysqli_close($con);
